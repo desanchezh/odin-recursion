63a047a878fb5538c99028fe8defb75fbf33d2ba
desanchezh <129230744+desanchezh@users.noreply.github.com>
2024-11-17 12:41:50 -0500

    Complete recursive fibonacci

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -1,18 +1,26 @@
-def fibs(n)
+def fibs(number)
   fib = []
   fib << 0
-  if n == 1
+  if number == 1
     fib
-  elsif n == 2
+  elsif number == 2
     fib << 1
   else
     fib << 1
-    (n - 2).times do
-      next_fib = fib[-2] + fib[-1]
-      fib << next_fib
+    (number - 2).times do
+      fib << (fib[-2] + fib[-1])
     end
   end
   p fib
 end
 
-fibs(8)
+def fibs_rec(number)
+  puts "This was printed recursively"
+  return [0] if number == 1
+  return [0, 1] if number == 2
+
+  fibs_arr = fibs_rec(number - 1)
+  fibs_arr << (fibs_arr[-2] + fibs_arr[-1])
+end
+
+p fibs_rec(4)
