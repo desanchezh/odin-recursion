5133c2a5aa206f0214512d6603b35e32396f0660
desanchezh <129230744+desanchezh@users.noreply.github.com>
2024-11-17 21:44:22 -0500

    Complete merge method

--- lib/merge_sort.rb
+++ lib/merge_sort.rb
@@ -1,1 +1,25 @@
-test
+arr = [3, 2, 1, 13, 8, 5, 0, 1]
+
+def merge(arr_a, arr_b)
+  i = 0
+  j = 0
+  k = 0
+  arr_c = []
+  while i < arr_a.length && j < arr_b.length
+    if arr_a[i] < arr_b[j]
+      arr_c[k] = arr_a[i]
+      i += 1
+    else
+      arr_c[k] = arr_b[j]
+      j += 1
+    end
+    k += 1
+  end
+  arr_c[k] = arr_a[i] if i < arr_a.length
+  arr_c[k] = arr_a[j] if j < arr_b.length
+  arr_c
+end
+
+arr_a = [1, 3, 10, 20]
+arr_b = [2, 6, 12, 15]
+p merge(arr_a, arr_b)
